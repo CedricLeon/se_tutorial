11f28bcb6c0687d050ebeffa6f32381021f53810
CedricLeon <leonard99cedric@gmail.com>
2023-10-19 15:04:27 +0200

    Writing the max_search() function and updating test

--- src/example_package/example.py
+++ src/example_package/example.py
@@ -2,3 +2,10 @@ import numpy as np
 def hello_world():
     x = np.zeros(5)
     print("hello world")
+
+def max_search(lst):
+    # Check if list is empty
+    if not lst or np.isnan(lst).any():
+        return float('nan')
+
+    return np.max(lst)
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,6 @@
 import pytest
-from src.example_package.example import max_search
+import numpy as np
+from example_package.example import max_search
 # Pytest is the simplest test workflow ever:
 #  - it works with assert
 #  - all methods strating with "test_" are runned
@@ -7,10 +8,19 @@ from src.example_package.example import max_search
 def test_main():
     assert True
 
-@pytest.mark.parametrize("lst", [[], [1,2,3,4,5], [1,1,1], [1,2,NaN,3]])
-
-def test_max_search(lst):
-    max = max_search()
-    assert max in lst
-    for i in lst:
-        assert max >= i
+@pytest.mark.parametrize("cases", [([], float('nan')),
+                                 ([1,2,3,4,5], 5),
+                                 ([1,1,1], 1),
+                                 ([1,2,float('nan'),3], float('nan'))])
+def test_max_search(cases):
+    lst, result = cases
+    max = max_search(lst)
+    # Check the weird cases
+    if np.isnan(max):
+        assert np.isnan(lst).any() or not lst
+    # Check classics
+    else:
+        assert max in lst
+        assert max == result
+        for i in lst:
+            assert max >= i
